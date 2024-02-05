303547a6fb3fe6f10bea23dde912615782833cbc
Ajmalsha Salim <ajmalaj8085@gmail.com>
2024-02-05 15:34:49 +0530

    Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "../src/layouts/dashboard";
+import { Auth } from "../src/layouts/auth";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -4,11 +4,13 @@ import Register from "../pages/User/Register"
 import Forgetpassword from "../pages/User/Forgetpassword"
 import Otp from "../pages/User/Otp"
 import Changepassword from "../pages/User/Changepassword"
+import Dashboard from "../../src/pages/dashboard/Home"
 let Routers = () => {
     return (
         <div className="Route">
 <Router>
     <Routes>
+        <Route path="/" element={<Dashboard/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/Register" element={<Register/>}/>
         <Route path="/Forgetpassword" element={<Forgetpassword/>}/>
