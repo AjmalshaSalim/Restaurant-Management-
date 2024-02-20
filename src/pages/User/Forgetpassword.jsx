354eda2a124f2711b21504b7824895de16cf67f9
micky mack <jijomonjs@gmail.com>
2024-02-20 18:29:31 +0530

    loader

--- src/pages/User/Forgetpassword.jsx
+++ src/pages/User/Forgetpassword.jsx
@@ -5,10 +5,12 @@ import { useNavigate } from 'react-router-dom';
 import { SEND_OTP } from '../../actions/AuthActions';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from '../../components/userloader/Loader.jsx';
 
 function Forgetpassword() {  
     const navigate = useNavigate();
     const [formData, setFormData] = useState({ phonenumber: '' });
+    const [loading,setLoading] = useState(false);
   
     const handleChange = (e) => {
         setFormData({
@@ -19,6 +21,7 @@ function Forgetpassword() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         try {
             const response = await SEND_OTP(formData);
             toast.success('OTP sent successfully!', {
@@ -42,43 +45,52 @@ function Forgetpassword() {
                 progress: undefined,
             });
             console.error('Error while sending phone number:', error.message);
+        } finally {
+            setLoading(false);
         }
     }
 
     return (
-        <div className="relative h-screen">
-            <img className="absolute inset-0 w-full h-full object-cover filter grayscale" src={BackgroundImage} alt="bg-image" />
-            <div className="absolute inset-0 bg-gradient-to-br from-black to-gray-800 opacity-60"></div>
-            <img className='h-auto w-[200px] md:w-[300px] ml-2 mt-2 absolute left-2 top-8' src={logo} alt='' />
-            <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-                <div className="w-full p-6 m-auto bg-gray-900 rounded-md shadow-md lg:max-w-xl opacity-70 border-1 border-gray-700">
-                    <h1 className="text-3xl font-semibold text-center text-white ">
-                        Forget Password
-                    </h1>
-                    <form onSubmit={handleSubmit} className="mt-6">
-                        <div className="mb-2">
-                            <label
-                                for="email"
-                                className="block text-sm font-semibold text-white"
-                            >
-                                Phone Number
-                            </label>
-                            <input
-                                onChange={handleChange} type="number" id="phonenumber" value={formData.phonenumber} required
-                                className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:red-purple-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40 hover:transform hover:scale-105 transition-transform duration-500 ease-in-out"
-                            />
-                        </div>
-                        <div className="mt-6 flex justify-center">
-                            <button type='submit' className=" w-auto px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-xl hover:bg-red-300 focus:outline-none focus:bg-red-800 ">
-                                Send OTP
-                            </button>
-                        </div>
-                    </form>
+        <>
+        {loading ? ( // Display the loader if loading is true
+            <Loader />
+        ) : (
+            <div className="relative h-screen">
+                <img className="absolute inset-0 w-full h-full object-cover filter grayscale" src={BackgroundImage} alt="bg-image" />
+                <div className="absolute inset-0 bg-gradient-to-br from-black to-gray-800 opacity-60"></div>
+                <img className='h-auto w-[200px] md:w-[300px] ml-2 mt-2 absolute left-2 top-8' src={logo} alt='' />
+                <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+                    <div className="w-full p-6 m-auto bg-gray-900 rounded-md shadow-md lg:max-w-xl opacity-70 border-1 border-gray-700">
+                        <h1 className="text-3xl font-semibold text-center text-white ">
+                            Forget Password
+                        </h1>
+                        <form onSubmit={handleSubmit} className="mt-6">
+                            <div className="mb-2">
+                                <label
+                                    for="email"
+                                    className="block text-sm font-semibold text-white"
+                                >
+                                    Phone Number
+                                </label>
+                                <input
+                                    onChange={handleChange} type="number" id="phonenumber" value={formData.phonenumber} required
+                                    className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:red-purple-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40 hover:transform hover:scale-105 transition-transform duration-500 ease-in-out"
+                                />
+                            </div>
+                            <div className="mt-6 flex justify-center">
+                                <button type='submit' className=" w-auto px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-red-700 rounded-xl hover:bg-red-300 focus:outline-none focus:bg-red-800 ">
+                                    Send OTP
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
+                {/* Toast Container for displaying messages */}
+                <ToastContainer />
             </div>
-            {/* Toast Container for displaying messages */}
-            <ToastContainer />
-        </div>
+        )}
+    </>
+       
     )
 }
 
