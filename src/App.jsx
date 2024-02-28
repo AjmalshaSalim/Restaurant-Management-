3f702423657d19c1f67a7ac67f4406846ba3e2f7
Ajmalsha Salim <ajmalaj8085@gmail.com>
2024-02-28 15:57:36 +0530

    equipments update

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import OwnerRegister from "../src/pages/Owner/auth/sign-up"
 import OwnerForgetpassword from "../src/pages/Owner/auth/Forgot-pw"
 import OwnerOtp from "../src/pages/Owner/auth/Otp"
 import OwnerChangepassword from "../src/pages/Owner/auth/Reset-pw"
+import OwnerUserList from "./pages/Owner/dashboard/UserList"
 import { PhoneNumberProvider } from "./context/phoneNumberContext"; // Import PhoneNumberProvider
 
 function App() {
@@ -56,6 +57,7 @@ function App() {
         <Route path ="/OwnerOtp" element={<OwnerOtp/>}/>
         <Route path="/OwnerChangepassword" element={<OwnerChangepassword/>}/>
         <Route path="/equipments/" element={<Equipments />} /> 
+        <Route path="/UserList/" element={<OwnerUserList />} />
       </Routes> 
     </PhoneNumberProvider>
   );
