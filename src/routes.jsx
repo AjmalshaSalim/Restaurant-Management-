cf9d15f19eda98381c4d0391389b134380c21750
ajmal <ajmalaj8085@gmail.com>
2024-04-17 15:33:13 +0530

    new changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -33,6 +33,9 @@ import ConfirmPayments from './pages/Owner/dashboard/ConfirmPayments';
 import {PaymentDetail} from './pages/Owner/dashboard/PaymentDetail';
 import {AllDietPlans} from './pages/Owner/dashboard/AllDietPlans';
 import {CreateDietPlans} from './pages/Owner/dashboard/CreateDietPlans'
+import TrainersList from './pages/Owner/dashboard/TrainersList';
+import AddTrainers from './pages/Owner/dashboard/AddTrainers';
+import SalaryManagement from './pages/Owner/dashboard/SalaryManagement';
 const icon = {
   className: 'w-5 h-5 text-inherit',
 };
@@ -96,6 +99,18 @@ export const routes = [
         element: <AllStaffs />,
       },
       {
+        icon: <UserPlusIcon {...icon} />,
+        name: 'Add Trainers',
+        path: '/AddTrainers',
+        element: <AddTrainers />,
+      },
+      {
+        icon: <FaUserTie {...icon} />,
+        name: 'All Trainers',
+        path: '/TrainersList',
+        element: <TrainersList />,
+      },
+      {
         icon: <BsPatchPlusFill {...icon} />,
         name: 'Create Plans',
         path: '/CreatePlans',
@@ -148,6 +163,12 @@ export const routes = [
         name: 'All Diet Plans',
         path: '/AllDietPlans',
         element: <AllDietPlans />,
+      },
+      {
+        icon: <FaDumbbell {...icon} />,
+        name: 'Salary Management',
+        path: '/SalaryManagement',
+        element: <SalaryManagement />,
       },
      
       // {
